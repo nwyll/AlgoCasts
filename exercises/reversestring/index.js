07b6aba57e1c5e0b398644e5ba2c7e5eb736d508
Natalie Wyll <nataliewyll@yahoo.com>
2018-08-18 17:59:40 +0200

    reverse string complete

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,23 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// const reverse = (str) => {
+//   return str.split('').reverse().join('');
+// };
+
+//alternative solution - using a loop
+// const reverse = (str) => {
+//   let reversed = '';
+//   for(let char of str) {
+//     reversed = char + reversed;
+//   }
+//   return reversed;
+// };
+
+//alternative solution - using reduce()
+const reverse = (str) => {
+  return str.split('').reduce((reversed, char) => reversed = char + reversed, '');
+};
+
 
 module.exports = reverse;
