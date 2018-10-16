7bab3c39248051f46aedd1f60a8b0715773d55c1
Natalie Wyll <nataliewyll@yahoo.com>
2018-10-16 23:59:51 +0200

    maxChars aboundgit add .!

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,24 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+const maxChar = (str) => {
+  const charMap = {};
+  let max = 0,
+      maxChar = '';
+
+  for (let char of str) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  //iterate thru charMap to find max value
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+};
 
 module.exports = maxChar;
