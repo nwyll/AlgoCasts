6211f7696d25b4c4e34729ad26eed0a0b15b1334
Natalie Wyll <nataliewyll@yahoo.com>
2018-10-18 23:45:58 +0200

    fizzbuzz strikes again

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,24 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+//most basic way, keep it simple
+//commment thought process as you are going through implementation
+const fizzBuzz = (n) => {
+  for (let i = 1; i <= n; i++) {
+    //check is value is divisible by  3 & 5
+    if ((i % 5 === 0) && (i % 3 === 0)) {
+      console.log('fizzbuzz');
+    } else if (i % 5 === 0) {
+      //check if value is divisible by 5
+      console.log('buzz');
+    } else if (i % 3 === 0) {
+      //check if value is divisible by 3
+      console.log('fizz');
+    } else {
+      //otherwise simply print the value
+      console.log(i);
+    }
+  }
+};
 
 module.exports = fizzBuzz;
