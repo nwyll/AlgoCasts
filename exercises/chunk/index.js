71ccf7116f9e3ad2a66b542bd41ec9ae2c66bcbc
Natalie Wyll <nataliewyll@yahoo.com>
2018-10-30 22:02:06 +0100

    All chunked up

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,39 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// const chunk = (array, size) => {
+//   //create a new empty array to add the chunks to
+//   let chunked = [];
+
+//   for (let element of array) {
+//     //get the last array in chunked
+//     const last = chunked[chunked.length - 1];
+
+//     //if the last array element does not exist (1st pass) or the subArray size is equal to the 
+//     //chunk size passed in, add a new subArray with the element included
+//     if (!last || last.length == size) {
+//       chunked.push([element]);
+//     } else {
+//       //otherwise add the element to the last subArray
+//       last.push(element);
+//     }
+//   }
+
+//   return chunked;
+// }
+
+//alt solution using .slice
+
+const chunk = (array, size) => {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+};
 
 module.exports = chunk;
