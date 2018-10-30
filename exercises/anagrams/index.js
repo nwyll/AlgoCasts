561fc334f3f9fab70f89fb0e4b0c0bae42c084a2
Natalie Wyll <nataliewyll@yahoo.com>
2018-10-30 22:58:22 +0100

    F'n use helper functions

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,46 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//   const charMapA = buildCharMap(stringA),
+//         charMapB = buildCharMap(stringB);
+    
+//   if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//     return false;
+//   }
+
+//   //to iterate over an object use 'in'
+//   for (let char in charMapA) {
+//     if (charMapA[char] !== charMapB[char]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// function buildCharMap(string) {
+//   const charMap = {};
+
+//   for (let char of string.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+
+//   return charMap;
+// }
+
+//alt solution using array.sort()
+function anagrams(stringA, stringB) {
+  return cleanUpString(stringA) === cleanUpString(stringB);
+}
+
+const cleanUpString = (string) => {
+  return string
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('');
+};
 
 module.exports = anagrams;
