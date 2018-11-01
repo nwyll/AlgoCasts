2ed92104552c78f4679b16c97f4ab89bae5adb42
Natalie Wyll <nataliewyll@yahoo.com>
2018-11-01 13:59:40 +0100

    Up we go

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,36 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+//iterative solution
+function steps(n) {
+  if (n <= 0 || typeof n != 'number') {
+    console.log('Please enter a number of steps greater than 0.')
+  }
+
+  for(let i = 1; i <= n; i++) {
+    const char = '#',
+          space = ' ';
+    console.log(char.repeat(i) + space.repeat(n - i));
+  }
+}
+
+//recursive solution
+// function steps(n, level = 1) {
+//   if (n <= 0 || typeof n != 'number') {
+//     console.log('Please enter a number of steps greater than 0.')
+//   }
+
+//   const char = '#',
+//         space = ' ';
+
+//   //establish base case
+//   if (level > n) return;
+
+//   //perform action
+//   console.log(char.repeat(level) + space.repeat(n - level));
+
+//   //call function again with new argument -> aproaching base case
+//   steps(3, level + 1);
+// }
 
 module.exports = steps;
