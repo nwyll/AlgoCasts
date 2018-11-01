095994806d079c1a1457f63b5c84eb68162fdd65
Natalie Wyll <nataliewyll@yahoo.com>
2018-11-01 14:20:58 +0100

    Stack 'm up

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -4,16 +4,46 @@
 // with N levels using the # character.  Make sure the
 // pyramid has spaces on both the left *and* right hand sides
 // --- Examples
-//   pyramid(1)
+//   pyramid(1) 1
 //       '#'
-//   pyramid(2)
+//   pyramid(2) 1 3 
 //       ' # '
 //       '###'
-//   pyramid(3)
+//   pyramid(3) 1 3 5
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4) 1 3 5 7
+//       '   #   '
+//       '  ###  '
+//       ' ##### '
+//       '#######' 
 
-function pyramid(n) {}
+//   pyramid(5) 1 3 5 7 9
+//   pyramid(6) 1 3 5 7 9 11
+//2n-1
+
+function pyramid(n) {
+  //validation
+  if (n <= 0 || typeof n != 'number') {
+    console.log('Please enter a number of steps greater than 0.')
+  }
+
+  //total string length = 2n - 1
+  //in a given row 
+    //the number of chars (#) is 2i - 1, starting at i = 1
+    //the number of spaces on each side is (total string length - number of chars) / 2
+
+  const totalStringLength = 2*n - 1,
+        char = '#',
+        space = ' ';
+  
+  for(let i = 1; i <= n; i++) {
+    const numChars = 2*i - 1;
+          numSpaces = (totalStringLength - numChars) / 2;
+
+    console.log(space.repeat(numSpaces) + char.repeat(numChars) + space.repeat(numSpaces));
+  }  
+}
 
 module.exports = pyramid;
